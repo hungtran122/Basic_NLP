a12376734a51ff39f6d2445296bf528301bb109a
Hung Tran <tmhung122@gmail.com>
2017-12-08 23:40:02 +0900

    add placeholder, embedding, loss_op, train_op

--- Word2Vec_with_TensorFlow.py
+++ Word2Vec_with_TensorFlow.py
@@ -7,7 +7,7 @@ import math
 import os
 import random
 import zipfile
-import nltk
+import platform
 from nltk.corpus import stopwords
 import time
 import numpy as np
@@ -15,6 +15,9 @@ from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 special_marks = [".",",",";","!","'",":"]
 class config:
     def __init__(self):
@@ -22,15 +25,42 @@ class config:
         for mark in special_marks:
             self.stopwords.append(mark)
         self.vocabulary_size = 200
-        self.filename = os.getcwd() + "\\data\\LoR_new"
+        if platform.system() == "Linux":
+            self.filename = os.getcwd() + "/Data/LoR_new"
+        else:
+            self.filename = os.getcwd() + "\\data\\LoR_new"
         self.batch_size = 12
         self.embedding_size = 128  # Dimension of the embedding vector.
-        self.skip_window = 3       # How many words to consider left and right.
+        self.skip_window = 1       # How many words to consider left and right.
         self.num_skips = 2        # How many times to reuse an input to generate a label.
+        self.num_sampled = 64
 class w2v_model():
     def __init__(self):
         self.config = config()
-    # Read the data into a list of strings.
+        self.add_placeholder()
+        self.embedding = self.add_embedding()
+        self.loss = self.add_loss_op()
+        self.train = self.add_train_op()
+        # Read the data into a list of strings.
+    def add_placeholder(self):
+        self.input_placeholder = tf.placeholder(dtype=tf.int32,shape=[self.config.batch_size])
+        self.label_placeholder = tf.placeholder(shape=[self.config.batch_size,None],dtype=tf.int32)
+        print(tf.shape(self.input_placeholder))
+    def add_embedding(self):
+        embedding = tf.Variable(tf.random_uniform([self.config.batch_size,self.config.embedding_size],-1.0, 1.0))
+        return tf.nn.embedding_lookup(embedding, self.input_placeholder)
+    def add_loss_op(self):
+        nce_weights = tf.Variable(tf.truncated_normal([self.config.batch_size, self.config.embedding_size],
+                                                      stddev=1.0/math.sqrt(self.config.embedding_size)))
+        nce_biases = tf.Variable(tf.zeros([self.config.batch_size]))
+        return tf.reduce_mean(tf.nn.nce_loss(weights = nce_weights,
+                                             biases = nce_biases,
+                                             inputs = self.embedding,
+                                             labels = self.label_placeholder,
+                                             num_sampled = self.config.num_sampled,
+                                             num_classes = self.config.batch_size))
+    def add_train_op(self):
+        return tf.train.GradientDescentOptimizer(1.0).minimize(self.loss)
     def read_data(self):
         processed_word_list = []
         start = time.time()
@@ -41,16 +71,16 @@ class w2v_model():
                 word = word.lower() # in case they arenet all lower cased
                 if word not in self.config.stopwords:
                     processed_word_list.append(word)
-        print ("Read data took {:.2f} second".format(time.time()-start))
-        print ("Size of vocabulary", len(processed_word_list), "including recurrence")
+        print("Read data took {:.2f} second".format(time.time()-start))
+        print("Size of vocabulary", len(processed_word_list), "including recurrence")
         #print ("Stop words contains: ", self.config.stopwords)
         return processed_word_list
-    def build_dataset(self,words): #words = processed word list
+    def build_dataset(self, words): #words = processed word list
         """Process raw inputs into a dataset."""
         count = [['UNK', -1]] # count is a list, each element is a list
         # print("count = ",type(count),count[0][0])
         # collect most common word, after this count size becomes n_words (50000)
-        count.extend(collections.Counter(words).most_common(self.config.vocabulary_size - 1)) 
+        count.extend(collections.Counter(words).most_common(self.config.vocabulary_size - 1))
         dictionary = dict()
         #each element in count has a word and occurences
         #store in dictionary with each word and its key
@@ -62,13 +92,18 @@ class w2v_model():
         i = 0
         #words is all word from training data with lenth 17005207
         #dictionary is a dict with length 50000
-        for word in words:
-            if word in dictionary:
-                index = dictionary[word]
-            else:
-                index = 0  # dictionary['UNK']
-                unk_count += 1
-            data.append(index) #storing index of dictionary
+#        for word in words:
+#            if word in dictionary:
+#                index = dictionary[word]
+#            else:
+#                index = 0  # dictionary['UNK']
+#                unk_count += 1
+#            data.append(index) #storing index of dictionary
+        try:
+            index = dictionary[word]
+        except KeyError:
+            index = 0
+            unk_count += 1
         #before assignment, count[0][1] = -1
         #after assigment, count[0][1] = 418391
         count[0][1] = unk_count
@@ -76,7 +111,7 @@ class w2v_model():
 
         return data, count, dictionary, reversed_dictionary
         # Step 3: Function to generate a training batch for the skip-gram model.
-    def generate_batch(self,data):
+    def generate_batch(self, data):
         global data_index
         num_skips = self.config.num_skips
         skip_window = self.config.skip_window
@@ -86,9 +121,13 @@ class w2v_model():
         batch = np.ndarray(shape=(batch_size), dtype=np.int32)
         labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
         span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+        print(span)
+        print(skip_window)
+        print('Length data ', len(data))
         buffer = collections.deque(maxlen=span)
         if data_index + span > len(data):
             data_index = 0
+        print('Data index', data_index)
         buffer.extend(data[data_index:data_index + span]) #copy data to buffer
         data_index += span
         for i in range(batch_size // num_skips):
@@ -115,23 +154,35 @@ class w2v_model():
         data_index = (data_index + len(data) - span) % len(data)
         return batch, labels
 
+data_index = 0
 def test_w2v():
-    w2v = w2v_model()
-    vocabulary = w2v.read_data()
-    data, count, dictionary, reversed_dictionary = w2v.build_dataset(vocabulary)
-    del vocabulary #reduce memory
-    print('Most common words (+UNK)', count[:5])
-    print('Sample data', data[:10], [reversed_dictionary[i] for i in data[:10]])
-    dictionary["history"]
-    batch, labels = w2v.generate_batch(data)
+    with tf.Graph().as_default():
+        init = tf.global_variables_initializer()
+        model = w2v_model()
+        num_steps = 1000
+        vocabulary = model.read_data()
+        data,count, dictionary, reverse_dictionary = model.build_dataset(vocabulary)
+        del vocabulary #reduce memory
+        print('Most common words', count[:5])
+        print('Sample data',data[:10], [reverse_dictionary[i] for i in data[:10]])
+        with tf.Session() as sess:
+            sess.run(init)
+            print("Initialized!")
+            average_loss = 0
+            for step in xrange(num_steps):
+                batch_inputs, batch_labels = model.generate_batch(data)
+
+
+#    dictionary["history"]
+#    batch, labels = w2v.generate_batch(data)
     # The data list stores index of word through whole corpus
     # for i in data:
     #     if i != 0:
     #         print(i, reversed_dictionary[i])
-    for i in range(w2v.config.batch_size):
-        print(reversed_dictionary[batch[i]],
-            '->', labels[i, 0], reversed_dictionary[labels[i, 0]])
+#    for i in range(w2v.config.batch_size):
+#        print(reversed_dictionary[batch[i]],
+#            '->', labels[i, 0], reversed_dictionary[labels[i, 0]])
         # print(batch[i])
-data_index = 0    
+data_index = 0
 if __name__ == "__main__":
     test_w2v()
